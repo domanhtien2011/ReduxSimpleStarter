2a87ca2cd4685b56cc53ff77cecec11f8cbfe074
Tien Manh Do <domanhtien2011@gmail.com>
2017-11-27 23:55:18 +0700

    Change eslint config to react style, adding onChange for react element

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
   render() {
     return (
       <div>
-        <input type="text" className='form-control' />
+        <input
+          type="text"
+          className="form-control"
+          onChange={event => console.log(event.target.value)}
+        />
       </div>
     );
   }
+
+
 }
 
 export default SearchBar;
