bb97318e70f244d934d053794fb9c56ccf080b3d
Tien Manh Do <domanhtien2011@gmail.com>
2017-11-27 23:31:58 +0700

    Add input in search_bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from './components/search_bar'
 
 var API_KEY = "AIzaSyAPJjQ7g8VsvFVFH7deYa3qQGYsdewjq-E";
 
 const App = () => {
-  return <div>Hi</div>;
+  return(
+    <div>
+      <SearchBar />
+    </div>
+    
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
