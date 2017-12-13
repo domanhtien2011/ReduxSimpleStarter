15710a07d5be7076ae41aa64e7207fc1247ab857
Tien Manh Do <domanhtien2011@gmail.com>
2017-12-13 21:36:17 +0700

    Working on video list component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import YTSeach from 'youtube-api-search';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list';
 
-var API_KEY = "AIzaSyAPJjQ7g8VsvFVFH7deYa3qQGYsdewjq-E";
+const API_KEY = "AIzaSyAPJjQ7g8VsvFVFH7deYa3qQGYsdewjq-E";
 
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-    
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    YTSeach({
+      key: API_KEY,
+      term: 'programming foundation'
+    }, (videos) => {
+      this.setState({videos});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos={this.state.videos}/>
+      </div>
+    )
+  };
 };
 
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(
+  <App/>, document.querySelector(".container"));
